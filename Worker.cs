74cd3e5ef846d319335b75e62a032b8d705dc4c4
Chris Craven <Chris.Craven@doane.edu>
2024-04-15 09:42:40 -0500

    Add project files.

---
+++ Worker.cs
@@ -0,0 +1,26 @@
+namespace _2Plus2
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                Console.WriteLine("Running Calculation");
+                int result = 2 + 2;
+
+                Console.WriteLine("2 + 2 = " + result); ;
+                
+                await Task.Delay(2000, stoppingToken);
+            }
+        }
+    }
+}
